e42c96791b6c4c82f49afa8094ac7cc8afbac0b5
Agnieszka <agnieszka.kozielska@op.pl>
2019-09-27 15:01:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$(".scroll-link").click(function(){
+	var containerName=$(this).attr("href").replace('#','.');
+	var containerPosition=$(containerName).offset().top-$(".top-bar").outerHeight(true);
+	console.log(containerPosition);
+	window.scrollTo(0,containerPosition);
+})
+
+//console.log($(".myContent"))
+$(".myContent").css("margin-top", $(".top-bar").outerHeight(true));
