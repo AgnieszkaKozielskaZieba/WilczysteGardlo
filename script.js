63d85cb4ea4361219ab561cc90226f8812a6f3ab
Agnieszka <agnieszka.kozielska@op.pl>
2019-09-30 12:43:13 +0200

    prettier

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-$(".scroll-link").click(function(){
-	var containerName=$(this).attr("href").replace('#','.');
-	var containerPosition=$(containerName).offset().top-$(".top-bar").outerHeight(true);
+$(".scroll-link").click(function() {
+	var containerName = $(this)
+		.attr("href")
+		.replace("#", ".");
+	var containerPosition =
+		$(containerName).offset().top - $(".top-bar").outerHeight(true);
 	console.log(containerPosition);
-	window.scrollTo(0,containerPosition);
-})
+	window.scrollTo(0, containerPosition);
+});
 
 //console.log($(".myContent"))
 $(".myContent").css("margin-top", $(".top-bar").outerHeight(true));
